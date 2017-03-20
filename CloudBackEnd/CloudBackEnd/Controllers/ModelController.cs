879e05841fb1c624586004b75c658b61e657ec3a
Stuart Schaefer <stscha@microsoft.com>
2017-03-20 11:39:36 -0700

    Re-adding model and controller.

---
+++ CloudBackEnd/CloudBackEnd/Controllers/ModelController.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using Swashbuckle.Swagger.Annotations;
+
+using System.Threading.Tasks;
+using System.IO;
+using System.Drawing;
+
+
+namespace CloudBackEnd.Controllers
+{
+    public class ModelController : ApiController
+    {
+        // GET api/values
+        [SwaggerOperation("GetAll")]
+        public async Task<HttpResponseMessage> Get()
+        {
+            string modelFilePath = AppDomain.CurrentDomain.BaseDirectory + "CNTK\\Models\\rps.model";
+
+            var response = new HttpResponseMessage(HttpStatusCode.OK)
+            {
+                Content = new StreamContent(File.OpenRead(modelFilePath))
+            };
+
+            response.Content.Headers.ContentType = new System.Net.Http.Headers.MediaTypeHeaderValue("application/binary");
+
+            return await Task.FromResult(response);
+        }
+
+        // PUT api/values/[GAMEID]
+        [SwaggerOperation("Update")]
+        [SwaggerResponse(HttpStatusCode.OK)]
+        [SwaggerResponse(HttpStatusCode.NotFound)]
+        public void Put(int gameId, [FromBody]string value)
+        {
+
+        }
+    }
+}
