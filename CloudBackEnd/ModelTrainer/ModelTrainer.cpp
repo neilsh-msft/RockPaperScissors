8715019778f2c85163bc9c0526ec92494afdfeb7
Stuart Schaefer <stscha@microsoft.com>
2017-03-27 15:58:07 -0700

    Temporary model creation to resolve execution problem.

--- CloudBackEnd/ModelTrainer/ModelTrainer.cpp
+++ CloudBackEnd/ModelTrainer/ModelTrainer.cpp
@@ -5,6 +5,7 @@
 #include "ModelTrainer.h"
 #include "TrainingData.h"
 #include <string>
+#include <functional>
 #include <iostream>
 
 using namespace std;
@@ -31,23 +32,123 @@ ModelTrainer::ModelTrainer(const wstring& modelFile, const wstring& dataFile)
 
 void ModelTrainer::LoadModel()
 {
-	// Load the model definition from a file
+	// We could create the model from scratch, but its schema is already defined by the model file...
 	_model = Function::LoadModel(_modelFile);
 
 	_inputs = _model->Arguments()[0];
-	_labels = _model->Outputs()[0];
+	_labels = _model->Output();
+}
 
-	// We could create the model from scratch, but its schema is already defined by the model file...
-	// _inputs = InputVariable({ 7 * LOOKBACK_MOVES }, CNTK::DataType::Float, L"Feature Vector");
-	// _labels = InputVariable({ 3 }, CNTK::DataType::Float, L"Labels");
-	// ...
+void ModelTrainer::CreateModel()
+{
+	// Define the model from scratch for training
+	size_t inputDim = 7 * LOOKBACK_MOVES;
+	size_t outputClasses = 3;
+	const size_t numHiddenLayers = HIDDEN_LAYERS;
+	size_t hiddenLayersDim = 1024;
+	CNTK::DeviceDescriptor device = DeviceDescriptor::DefaultDevice();
+
+	_inputs = InputVariable({ inputDim }, CNTK::DataType::Float, L"Feature Vector");
+	_labels = InputVariable({ outputClasses }, CNTK::DataType::Float, L"Labels");
+	
+	// Define model parameters that should be shared among evaluation requests against the same model
+	auto inputTimesParam = Parameter(NDArrayView::RandomUniform<float>({ hiddenLayersDim, inputDim }, -0.5, 0.5, 1, device));
+	auto inputPlusParam = Parameter({ hiddenLayersDim }, 0.0f, device);
+	Parameter hiddenLayerTimesParam[numHiddenLayers - 1] = {
+		Parameter(NDArrayView::RandomUniform<float>({ hiddenLayersDim, hiddenLayersDim }, -0.5, 0.5, 1, device)),
+		Parameter(NDArrayView::RandomUniform<float>({ hiddenLayersDim, hiddenLayersDim }, -0.5, 0.5, 1, device)),
+		Parameter(NDArrayView::RandomUniform<float>({ hiddenLayersDim, hiddenLayersDim }, -0.5, 0.5, 1, device)),
+		Parameter(NDArrayView::RandomUniform<float>({ hiddenLayersDim, hiddenLayersDim }, -0.5, 0.5, 1, device)),
+		Parameter(NDArrayView::RandomUniform<float>({ hiddenLayersDim, hiddenLayersDim }, -0.5, 0.5, 1, device)),
+		Parameter(NDArrayView::RandomUniform<float>({ hiddenLayersDim, hiddenLayersDim }, -0.5, 0.5, 1, device)),
+		Parameter(NDArrayView::RandomUniform<float>({ hiddenLayersDim, hiddenLayersDim }, -0.5, 0.5, 1, device)),
+		Parameter(NDArrayView::RandomUniform<float>({ hiddenLayersDim, hiddenLayersDim }, -0.5, 0.5, 1, device)),
+		Parameter(NDArrayView::RandomUniform<float>({ hiddenLayersDim, hiddenLayersDim }, -0.5, 0.5, 1, device))
+	};
+	Parameter hiddenLayerPlusParam[numHiddenLayers - 1] = {
+		Parameter({ hiddenLayersDim }, 0.0f, device),
+		Parameter({ hiddenLayersDim }, 0.0f, device),
+		Parameter({ hiddenLayersDim }, 0.0f, device),
+		Parameter({ hiddenLayersDim }, 0.0f, device),
+		Parameter({ hiddenLayersDim }, 0.0f, device),
+		Parameter({ hiddenLayersDim }, 0.0f, device),
+		Parameter({ hiddenLayersDim }, 0.0f, device),
+		Parameter({ hiddenLayersDim }, 0.0f, device),
+		Parameter({ hiddenLayersDim }, 0.0f, device),
+	};
+	auto outputTimesParam = Parameter(NDArrayView::RandomUniform<float>({ outputClasses, hiddenLayersDim }, -0.5, 0.5, 1, device));
+
+	_model = FullyConnectedFeedForwardClassifierNetWithSharedParameters(_inputs,
+																		numHiddenLayers,
+																		inputTimesParam,
+																		inputPlusParam,
+																		hiddenLayerTimesParam,
+																		hiddenLayerPlusParam,
+																		outputTimesParam,
+																		std::bind(Sigmoid, std::placeholders::_1, L""));
+}
+
+inline FunctionPtr ModelTrainer::FullyConnectedFeedForwardClassifierNetWithSharedParameters(Variable input,
+	size_t numHiddenLayers,
+	const Parameter& inputTimesParam,
+	const Parameter& inputPlusParam,
+	const Parameter hiddenLayerTimesParam[],
+	const Parameter hiddenLayerPlusParam[],
+	const Parameter& outputTimesParam,
+	const std::function<FunctionPtr(const FunctionPtr&)>& nonLinearity)
+{
+	assert(numHiddenLayers >= 1);
+	auto classifierRoot = FullyConnectedDNNLayerWithSharedParameters(input, inputTimesParam, inputPlusParam, nonLinearity);
+
+	for (size_t i = 1; i < numHiddenLayers; ++i)
+	{
+		classifierRoot = FullyConnectedDNNLayerWithSharedParameters(classifierRoot, hiddenLayerTimesParam[i - 1], hiddenLayerPlusParam[i - 1], nonLinearity);
+	}
+
+	// Todo: assume that outputTimesParam has matched output dim and hiddenLayerDim
+	classifierRoot = Times(outputTimesParam, classifierRoot);
+	return classifierRoot;
+}
+
+// Taken from the CNTK Examples
+inline FunctionPtr ModelTrainer::FullyConnectedDNNLayerWithSharedParameters(Variable input,
+	const Parameter& timesParam,
+	const Parameter& plusParam,
+	const std::function<FunctionPtr(const FunctionPtr&)>& nonLinearity)
+{
+	assert(input.Shape().Rank() == 1);
+
+	// Todo: assume that timesParam has matched outputDim and inputDim 
+	auto timesFunction = Times(timesParam, input);
+
+	// Todo: assume that timesParam has matched outputDim 
+	auto plusFunction = Plus(plusParam, timesFunction);
+
+	return nonLinearity(plusFunction);
+}
+
+inline FunctionPtr ModelTrainer::SetupFullyConnectedLinearLayer(Variable input, size_t outputDim, const DeviceDescriptor& device, const std::wstring& outputName)
+{
+	assert(input.Shape().Rank() == 1);
+	size_t inputDim = input.Shape()[0];
+
+	auto timesParam = CNTK::Parameter(CNTK::NDArrayView::RandomUniform<float>({ outputDim, inputDim }, -0.05, 0.05, 1, device));
+	auto timesFunction = CNTK::Times(timesParam, input);
+
+	auto plusParam = CNTK::Parameter(CNTK::NDArrayView::RandomUniform<float>({ outputDim }, -0.05, 0.05, 1, device));
+	return CNTK::Plus(plusParam, timesFunction, outputName);
+}
+
+inline FunctionPtr ModelTrainer::SetupFullyConnectedDNNLayer(Variable input, size_t outputDim, const DeviceDescriptor& device, const std::function<FunctionPtr(const FunctionPtr&)>& nonLinearity)
+{
+	return nonLinearity(SetupFullyConnectedLinearLayer(input, outputDim, device));
 }
 
 CNTK::TrainerPtr ModelTrainer::CreateTrainerForModel()
 {
 	// Create a trainer
 	auto lossFunction = CrossEntropyWithSoftmax(_model->Output(), _labels, L"Loss Function");
-	auto evalFunction = ClassificationError(_model->RootFunction(), _labels, L"Error");
+	auto evalFunction = ClassificationError(_model->Output(), _labels, L"Classification Error");
 	auto learningFunction = SGDLearner(_model->Parameters(), LearningRateSchedule(0.125, LearningRateSchedule::UnitType::Minibatch));
 	return CreateTrainer(_model, lossFunction, evalFunction, { learningFunction });
 }
@@ -58,7 +159,7 @@ void ModelTrainer::Train()
 	TrainingData *loader = new TrainingData();
 	loader->LoadFromFile(_dataFile, trainingData);
 
-	LoadModel();
+	CreateModel();
 	auto trainer = CreateTrainerForModel();
 
 	int minibatchSize = GAME_LENGTH;
@@ -92,9 +193,9 @@ void ModelTrainer::Train()
 		trainer->TrainMinibatch(arguments, DeviceDescriptor::DefaultDevice());
 		
 		// TODO: Output some progress data....
-		cout << "Minibatch: " << trainer->PreviousMinibatchSampleCount() << ", Loss: " << trainer->PreviousMinibatchLossAverage() << ", Error: " << trainer->PreviousMinibatchEvaluationAverage();
+		cout << "Minibatch: " << i << ", Loss: " << trainer->PreviousMinibatchLossAverage() << ", Error: " << trainer->PreviousMinibatchEvaluationAverage() * 100 << "%\n";
 		trainingPosition += minibatchSize;
 	}
 
-	// trainer->Model()->SaveModel(_modelFile);
+	trainer->Model()->SaveModel(_modelFile);
 }
--- CloudBackEnd/ModelTrainer/ModelTrainer.h
+++ CloudBackEnd/ModelTrainer/ModelTrainer.h
@@ -18,7 +18,23 @@ namespace CntkTraining
 
 	protected:
 		void LoadModel();
+		void CreateModel();
 		CNTK::TrainerPtr CreateTrainerForModel();
+
+		CNTK::FunctionPtr FullyConnectedFeedForwardClassifierNetWithSharedParameters(CNTK::Variable input,
+			size_t numHiddenLayers,
+			const CNTK::Parameter& inputTimesParam,
+			const CNTK::Parameter& inputPlusParam,
+			const CNTK::Parameter hiddenLayerTimesParam[],
+			const CNTK::Parameter hiddenLayerPlusParam[],
+			const CNTK::Parameter& outputTimesParam,
+			const std::function<CNTK::FunctionPtr(const CNTK::FunctionPtr&)>& nonLinearity);
+		CNTK::FunctionPtr FullyConnectedDNNLayerWithSharedParameters(CNTK::Variable input, const CNTK::Parameter& timesParam, const CNTK::Parameter& plusParam,
+			const std::function<CNTK::FunctionPtr(const CNTK::FunctionPtr&)>& nonLinearity);
+		CNTK::FunctionPtr SetupFullyConnectedLinearLayer(CNTK::Variable input, size_t outputDim, const CNTK::DeviceDescriptor& device,
+			const std::wstring& outputName = L"");
+		CNTK::FunctionPtr SetupFullyConnectedDNNLayer(CNTK::Variable input, size_t outputDim, const CNTK::DeviceDescriptor& device,
+			const std::function<CNTK::FunctionPtr(const CNTK::FunctionPtr&)>& nonLinearity);
 
 		CNTK::FunctionPtr _model;
 		CNTK::Variable _inputs, _labels;
